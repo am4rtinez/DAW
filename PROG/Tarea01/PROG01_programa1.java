139918d3f165e768167d8d3451849a293198a95c
am4rtinez <anglxd@gmail.com>
2020-11-07 19:58:07 +0100

    Refactor code

--- PROG/Tarea01/PROG01_programa1.java
+++ PROG/Tarea01/PROG01_programa1.java
@@ -1,16 +1,13 @@
 public class PROG01_programa1 {
-    public static void main(String[] args) {
-      System.out.println("Ciclo formativo - DESARROLLO DE APLICACIONES WEB");
-      System.out.println("Módulo Profesional - PROGRAMACIÓN");
-      System.out.println("UNIDAD DE TRABAJO 01");
-      System.out.println("Introducción a la programación");
-      System.out.println("Ángel Martí­nez Rodrí­guez");
-      System.out.println("Palma / Illes Balears");
-      System.out.println("07/11/2020");
-      System.out.println("programa1");
-      System.out.println("Ejecutado desde NetBeans");
-    }
+  public static void main(String[] args) {
+    System.out.println("Ciclo formativo - DESARROLLO DE APLICACIONES WEB");
+    System.out.println("Mï¿½dulo Profesional - PROGRAMACIï¿½N");
+    System.out.println("UNIDAD DE TRABAJO 01");
+    System.out.println("Introducciï¿½n a la programaciï¿½n");
+    System.out.println("ï¿½ngel Martï¿½ï¿½nez Rodrï¿½ï¿½guez");
+    System.out.println("Palma / Illes Balears");
+    System.out.println("07/11/2020");
+    System.out.println("programa1");
+    System.out.println("Ejecutado desde NetBeans");
+  }
 }
-  
-  
-  
