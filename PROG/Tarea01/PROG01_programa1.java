992923efe40686fa3f5c93a013b83b55a21a7197
am4rtinez <anglxd@gmail.com>
2020-11-07 19:48:08 +0100

    Carga tarea 1 de programación

---
+++ PROG/Tarea01/PROG01_programa1.java
@@ -0,0 +1,16 @@
+public class PROG01_programa1 {
+    public static void main(String[] args) {
+      System.out.println("Ciclo formativo - DESARROLLO DE APLICACIONES WEB");
+      System.out.println("Módulo Profesional - PROGRAMACIÓN");
+      System.out.println("UNIDAD DE TRABAJO 01");
+      System.out.println("Introducción a la programación");
+      System.out.println("Ángel Martí­nez Rodrí­guez");
+      System.out.println("Palma / Illes Balears");
+      System.out.println("07/11/2020");
+      System.out.println("programa1");
+      System.out.println("Ejecutado desde NetBeans");
+    }
+}
+  
+  
+  
