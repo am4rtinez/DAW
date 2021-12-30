933f1c845ac44f0f98899d8be7c4a34a706c9a7a
Angel Martinez <anglxd@gmail.com>
2021-12-30 21:19:38 +0100

    Finalizar ejercicio 3.2

--- DWEC/DWEC03/Codigo/exercicis/altres.js
+++ DWEC/DWEC03/Codigo/exercicis/altres.js
@@ -1,6 +1,7 @@
 window.onload = () => {
     const nmax = 10; // constante que contiene el numero de maximo. En este caso de 1 a 10.
     arrels(nmax, "arrels") // Calcula y pinta las raices cuadradas.
+    maxim("maxim")
 }
 
 /* Funcion que calcula y pinta las raices cuadradas.
@@ -14,4 +15,20 @@ function arrels(n, parent) {
         p.appendChild(t);
         document.getElementById(parent).appendChild(p);
     }
+}
+
+function maxim(parent) {
+    let nums = prompt("Insertar numeros separados por espacio: ");
+    let sarray = nums.split(" ")
+    let maxim = Math.max(...sarray)
+
+    let p1 = document.createElement("p");
+    let t1 = document.createTextNode("Numeros introducidos: " + sarray);
+    let p2 = document.createElement("p");
+    let t2 = document.createTextNode("Maxim: " + maxim);
+    
+    p1.appendChild(t1);
+    p2.appendChild(t2);
+    document.getElementById(parent).appendChild(p1);
+    document.getElementById(parent).appendChild(p2);
 }
