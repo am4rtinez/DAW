7cb405462e40fa019738934696e342786f7642a2
am4rtinez <anglxd@gmail.com>
2022-04-01 01:40:55 +0200

    añadir metodos en botones

--- DIW/DIW06/Codigo/js/app.js
+++ DIW/DIW06/Codigo/js/app.js
@@ -18,4 +18,31 @@ $(function () {
         e.preventDefault();
         $(".img-fadeout").fadeIn();
     });
+
+    $('#fadetoggle').click(function (e) { 
+        e.preventDefault();
+        $(".img-fadeout").fadeToggle(3000);
+    });
+
+    $("#fadeto").click(function(){
+        let img =  $(".img-fadeout")
+        console.log(img.attr('faded'))
+        if (img.attr('faded') == "false"){
+            img.fadeTo(1000, 0.4);
+            img.attr('faded', true)
+        } else {
+            img.fadeTo(1000, 1);
+            img.attr('faded', false)
+        }
+    });
+
+    $('#hide').click(function (e) { 
+        e.preventDefault();
+        $(".img-fadeout").hide();
+    });
+
+    $('#show').click(function (e) { 
+        e.preventDefault();
+        $(".img-fadeout").show();
+    });
 })
