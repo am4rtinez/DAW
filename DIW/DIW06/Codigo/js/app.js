45b7e06d8e44abaf2df5504ea2708e158bb27175
am4rtinez <anglxd@gmail.com>
2022-03-31 03:00:42 +0200

    Continuacion DIW06

--- DIW/DIW06/Codigo/js/app.js
+++ DIW/DIW06/Codigo/js/app.js
@@ -1,3 +1,11 @@
 $(function () {
-    
+    const primaryNav = $(".primary-navigation");
+    const navToggle = $(".nav-toggle");
+    const navIcon = $('#nav-icon')
+
+    navToggle.click(function (e) { 
+        e.preventDefault();
+        navIcon.toggleClass("fa-times");
+        primaryNav.slideToggle();
+    });
 })
