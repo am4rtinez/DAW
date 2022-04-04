6ff7599982438a4e91a6450ced8a063ecc95d612
am4rtinez <anglxd@gmail.com>
2022-04-04 15:19:07 +0200

    Nuevas funcionalidades

--- DIW/DIW06/Codigo/js/app.js
+++ DIW/DIW06/Codigo/js/app.js
@@ -1,31 +1,31 @@
 /**
  * DONE: Mètodes d'advents de jQuery (Els heu de fer tots): (2,4p) ◦
- * on(), 
- * off(), 
- * one() (0,6p)◦
+ * DONE: on(), 
+ * DONE: off(), 
+ * DONE: one() (0,6p)◦
  * DONE: click(), 
- * dblclick(), 
+ * DONE: dblclick(), 
  * mouseenter(), 
  * mouseleave() (0,2p)
  * DONE: Keypress(), 
  * DONE: keydown(), 
  * DONE: keyup(). (0,6p)
  * focus(), 
- * focusin(), 
- * focusout(). (0,6p)
+ * DONE: focusin(), 
+ * DONE: focusout(). (0,6p)
  * resize(), 
  * scroll() (0,4p)
  * 
  * DONE: Efectes (Els heu de fer tots) (3,3p)
  * DONE: fadein(), fadeout(), fadetoggle(), fadeTo().  (0,8p). (0,2 respectivament)
- * slideDown(), 
- * slideUp(), 
+ * DONE: slideDown(), 
+ * DONE: slideUp(), 
  * DONE: slideToggle(). (0,6p). (0,2 resp)
  * animate():(0,6p)
  *  amb valors relatius. (0,2p)
  *  amb valors predefinits. (0,2p)
  *  amb funcionalitat de cua. (0,2p)
- * stop(). (0,2p)◦
+ * DONE: stop(). (0,2p)◦
  * DONE: hide(), 
  * DONE: show(),  (0,4p).
  * DONE: Utilització de seqüències de funcions (Callback). (0,5p)
@@ -59,15 +59,15 @@ $(function () {
 
     $(".img-fadeout").click(function (e) { 
         e.preventDefault();
-        $(this).fadeOut();
+        $(this).fadeOut(500);
     });
 
     $('#restore').click(function (e) { 
         e.preventDefault();
-        $(".img-fadeout").fadeIn();
+        $(".img-fadeout").fadeIn(500);
     });
 
-    $('#fadetoggle').click(function (e) { 
+    $('#fadetoggle').on("click", function (e) { 
         e.preventDefault();
         $(".img-fadeout").fadeToggle(3000);
     });
@@ -86,22 +86,77 @@ $(function () {
 
     $('#hide').click(function (e) { 
         e.preventDefault();
-        $(".img-fadeout").hide();
+        $(".img-fadeout").hide(1000);
     });
 
     $('#show').click(function (e) { 
         e.preventDefault();
-        $(".img-fadeout").show();
+        $(".img-fadeout").show(1000);
     });
 
-    $('#cercador').keypress(function (e) { 
-        $(".contador").text(contador += 1);
+    $('#cercador')
+        .keypress(function (e) { 
+            $(".contador").text(contador += 1);
+        })
+        .focusin(function(){
+            $(this).css("background", "#D8E0E7")
+        })
+        .focusout(function(){
+            $(this).css("background", "#fff")
+            $(this).css("border", "solid 1px black")
+        })
+        .keydown(function (e) { 
+            if ( (e.code == "KeyA") || (e.code == "KeyE") || (e.code == "KeyI") || (e.code == "KeyO") || (e.code == "KeyU")) {
+                $(".vcontador").text(vcontador += 1);
+            }
+        });
+
+    $('#off-img-click').click(function (e) { 
+        e.preventDefault();
+        $(".img-fadeout").off()
     });
 
-    $('#cercador').keydown(function (e) { 
-        if ( (e.code == "KeyA") || (e.code == "KeyE") || (e.code == "KeyI") || (e.code == "KeyO") || (e.code == "KeyU")) {
-            $(".vcontador").text(vcontador += 1);
-        }
+    $('#info').one("click",function (e) { 
+        e.preventDefault();
+        alert("Clicar sobre uno de los emojis hara que desaparezca. FadeOut(). \n" + 
+                "Clicar sobre el boton Restaura imagenes, restaurara las imagenes. FadeIn(). \n" + 
+                "Clicar sobre el boton Muesta/Oculta hará que aparezcan/desaparezcan los emojis.\n" +
+                "Clicar sobre el boton Transparencia hará que los emojis pierdan o ganen opacidad.\n" +
+                "Clicar sobre el boton Ocultar hará que los emojis desaparezcan.\n" + 
+                "Clicar sobre el boton Mostrar hará que los emojis reaparezcan.\n" +
+                "Clicar sobre el boton Off hará que el click sobre los emojis no funcione.\n" +
+                "Clicar sobre el boton Info mostrará este alert una sola vez."
+            )
+    });
+
+    $('#rotate').click(function (e) { 
+        e.preventDefault();
+        $('.sun').animate(
+            // {  transform: degree },
+            {degrees: 359},
+            {
+                duration: 3500,
+                easing: "linear",
+                step: function(now,fx) {
+                    $(this).css({
+                        transform : 'rotate('+now+'deg)'
+                    });
+                }
+            }
+        );
+    });
+
+    $('#stop').click(function (e) { 
+        e.preventDefault();
+        $('.sun').stop();
+    });
+
+    $("#flip").click(function(){
+        $("#panel").slideDown(2000)
+    });
+    
+    $("#flip").dblclick(function(){
+        $("#panel").slideUp(2000)
     });
 })
 
@@ -161,4 +216,22 @@ createButton = (clase, text, url, id) => {
         window.location.assign(`${url}?idEdit=${id}`); // Navega a la URL asignando el parametro idEdit.
     });
     return button
+}
+
+function AnimateRotate(angle) { 
+    // caching the object for performance reasons 
+    var $elem = $('.sun'); 
+    // we use a pseudo object for the animation 
+    // (starts from `0` to `angle`), you can name it as you want 
+    $({deg: 0}).animate({deg: angle}, { 
+        duration: 2000, 
+        step: function(now) { 
+            // in the step-callback (that is fired each step of the animation), 
+            // you can use the `now` paramter which contains the current 
+            // animation-position (`0` up to `angle`) 
+            $elem.css({ 
+                transform: 'rotate(' + now + 'deg)' 
+            })
+        }
+    })
 }
