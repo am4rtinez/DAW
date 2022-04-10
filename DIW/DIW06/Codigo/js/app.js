122c139fef379ce0f9c83d6a690e185034077d18
am4rtinez <anglxd@gmail.com>
2022-04-10 12:30:18 +0200

    Reorganizar proyecto

--- DIW/DIW06/Codigo/js/app.js
+++ DIW/DIW06/Codigo/js/app.js
@@ -1,178 +1,347 @@
 /**
- * DONE: Mètodes d'advents de jQuery (Els heu de fer tots): (2,4p) ◦
- * DONE: on(), 
- * DONE: off(), 
- * DONE: one() (0,6p)◦
- * DONE: click(), 
- * DONE: dblclick(), 
- * DONE: mouseenter(), 
- * DONE: mouseleave() (0,2p)
- * DONE: Keypress(), 
- * DONE: keydown(), 
- * DONE: keyup(). (0,6p)
- * focus(), 
- * DONE: focusin(), 
- * DONE: focusout(). (0,6p)
- * resize(), 
- * scroll() (0,4p)
- * 
- * DONE: Efectes (Els heu de fer tots) (3,3p)
- * DONE: fadein(), fadeout(), fadetoggle(), fadeTo().  (0,8p). (0,2 respectivament)
- * DONE: slideDown(), 
- * DONE: slideUp(), 
- * DONE: slideToggle(). (0,6p). (0,2 resp)
- * DONE: animate():(0,6p)
- *  amb valors relatius. (0,2p)
- *  amb valors predefinits. (0,2p)
- *  amb funcionalitat de cua. (0,2p)
- * DONE: stop(). (0,2p)◦
- * DONE: hide(), 
- * DONE: show(),  (0,4p).
- * DONE: Utilització de seqüències de funcions (Callback). (0,5p)
- * DONE: Encadenament() (0,2p)
- * 
- * DONE: jQuery HTML (Els heu de fer tots): (1,3p)
- * DONE: append(), 
- * prepend(), 
- * after(), 
- * before(). (0,8p)
- * DONE: Manipulació CSS. (0,5p)
+ * TODO: resize(), 
+ * TODO: scroll() (0,4p)
  */
 
- const server = "52.178.39.51:8080"
+const server = "52.178.39.51:8080"
+const imagenes = ['frej2011', 'frej2015', 'frej2016', 'frej2017', 'frej2018', 'frej2019']
+let interval
+let pos = 0
+let $rotator
 
 $(function () {
+    const primaryNav = $(".primary-navigation")
+    const navToogle = $(".nav-toggle")
     const list = $("#llista")
     const input = $('#cercador')
     let contador = 0
     let vcontador = 0
     
-    // Loop para animar el logo
-    opacityLoop()
+    //Hace el set de los titles para que aparezca la info cuando se pasa el raton encima.
+    setTitle('#logo', 'Animate: Modifica la opacidad del logo.\nCon un click para la animación.\nCon doble click vuelve a realizar la animación.')
+    setTitle('.fa-ship', 'toogleClass: Cambia la clase para que tenga color al mostrar el panel.\nslideToggle: Muestra/oculta panel con una imagen.')
+    setTitle('.fa-shield-halved', 'toogleClass: Cambia la clase para que tenga color al mostrar el panel.\nslideToggle: Muestra/oculta panel con una imagen.')
+    setTitle('.fa-cross', 'toogleClass: Cambia la clase para que tenga color al mostrar el panel.\nslideToggle: Muestra/oculta panel con una imagen.')
+    setTitle('#cercador', 'FocusIn: Cambia el color del fondo del input.\nFocusOut: Restablece el color de fondo.\nKeyPress: Cuenta teclas presionadas.\nKeyUp: Busca editores\nKeyDown: Cuenta las vocales presionadas.')
+    setTitle('#off-img-click', 'Off: Detiene el click en las imagenes.')
+    setTitleImg('.content-img', '.content-section', 'Click en la imagen hace que desaparezca (fadeOut).', 'Doble click en la seccion y reaparece la imagen (fadeIn).')
+    setTitle('#previous', 'Retrocede una imagen.')
+    setTitle('#play', 'Activa el slider de las imagenes.')
+    setTitle('#pause', 'Pausa el slider de las imagenes.')
+    setTitle('#stop', 'Para el slider de las imagenes. Reinicia.')
+    setTitle('#next', 'Avanza una imagen.')
+    setTitle('#flip', 'Muestra/oculta un panel mágico. slideDown() y slideUp().')
+    setTitle('#info', 'Muestra un alert con información una única vez (one()).')
+    setTitle('#show', 'Muestra el boton info nuevamente (show()).')
+    setTitle('#rotate', 'Activa la animación del sol.')
+    setTitle('#stop-animation', 'Para la animación del sol.')
 
+    // Hace la busqueda de editores segun lo insertado en el input.
     search(list, input)
 
-    $(".nav-toggle").click(function (e) { 
+    // Loop para animar el logo
+    opacityLoop($('#logo'))
+
+    //Controla los eventos de click y doble click del logo para animarlo o no.
+    $('#logo')
+        .click(function (e) { 
+            e.preventDefault();
+            pauseOpacityLoop($(this))
+        })
+        .dblclick(function (e) {
+            e.preventDefault();
+            opacityLoop($(this))
+        })
+    
+    // Oculta el boton show inicialmente.
+    $('#show').hide()
+
+    navToogle.click(function (e) { 
         e.preventDefault();
+        let visibility = primaryNav.attr('data-visible');
         $('#nav-icon').toggleClass("fa-times");
-        $(".primary-navigation").slideToggle();
+        if (visibility === "false") {
+            primaryNav.attr('data-visible', true);
+        } else {
+            primaryNav.attr('data-visible', false);
+        }
+    });
+    
+    $('.fa-ship').click(function (e) { 
+        e.preventDefault();
+        $(this).toggleClass('red')
+        $("#panel").slideToggle(2000)
     });
 
-    $(".img-fadeout").click(function (e) { 
+    $('.fa-shield-halved').click(function (e) { 
+        e.preventDefault();
+        $(this).toggleClass('blue')
+        $("#panel2").slideToggle(2000)
+    });
+
+    $('.fa-cross').click(function (e) { 
+        e.preventDefault();
+        $(this).toggleClass('gold')
+        $("#panel3").slideToggle(2000)
+    });
+
+    $('#play').click(function (e) {
+        do_slide($('#image-no'), $('.gallery-img'))
+        setFaded($(this), true)
+        setFaded($('#stop'), false)
+        setFaded($('#pause'), false)
+    })
+
+    $('#pause').click(function (e) {
+        pause_slide()
+        setFaded($(this), true)
+        setFaded($('#play'), false)
+        setFaded($('#stop'), true)
+    })
+
+    $('#stop').click(function (e) {
+        stop_slide($('#image-no'))
+        setFaded($(this), true)
+        setFaded($('#play'), false)
+        setFaded($('#pause'), true)
+    })
+
+    $('#previous').click(function (e) {
+        previousImage()
+    })
+
+    $('#next').click(function (e) {
+        nextImage($('#image-no'), $('.gallery-img'))
+    })
+    
+    /**
+     * Cuando se hace click sobre la imagen con la clase .content-img esta desaparece.
+     */
+    $(".content-img").on('click', function (e) { 
         e.preventDefault();
         $(this).fadeOut(500);
     });
 
-    $('#restore').click(function (e) { 
+    /**
+     * Cuando se hace doble clic sobre la section (.content-section) aparece la imagen de la sección.
+     */
+    $('.content-section').dblclick(function (e) { 
         e.preventDefault();
-        $(".img-fadeout").fadeIn(500);
-    });
-
-    $('#fadetoggle').on("click", function (e) { 
-        e.preventDefault();
-        $(".img-fadeout").fadeToggle(3000);
-    });
-
-    $("#fadeto").click(function(){
-        let img =  $(".img-fadeout")
-        console.log(img.attr('faded'))
-        if (img.attr('faded') == "false"){
-            img.fadeTo(1000, 0.4);
-            img.attr('faded', true)
-        } else {
-            img.fadeTo(1000, 1);
-            img.attr('faded', false)
-        }
-    });
-
-    $('#hide').click(function (e) { 
-        e.preventDefault();
-        $(".img-fadeout").hide(1000);
-    });
-
-    $('#show').click(function (e) { 
-        e.preventDefault();
-        $(".img-fadeout").show(1000);
+        $(this).find('.content-img').fadeIn(500)
     });
 
     $('#cercador')
+        .focusin(function (){
+            $(this).css('background-color', '#f3f35d')
+        })
+        .focusout(function (){
+            $(this).css('background-color', '#fff')
+        })
         .keypress(function (e) { 
             $(".contador").text(contador += 1);
         })
-        .focusin(function(){
-            $(this).css("background", "#D8E0E7")
-        })
-        .focusout(function(){
-            $(this).css("background", "#fff")
-            $(this).css("border", "solid 1px grey")
-        })
         .keydown(function (e) { 
-            if ( (e.code == "KeyA") || (e.code == "KeyE") || (e.code == "KeyI") || (e.code == "KeyO") || (e.code == "KeyU")) {
+            if ((e.code == "KeyA") || (e.code == "KeyE") || (e.code == "KeyI") || (e.code == "KeyO") || (e.code == "KeyU")) {
                 $(".vcontador").text(vcontador += 1);
             }
         });
 
+    // Si se clica sobre este elemento pausa el evento de las imagenes.
     $('#off-img-click').click(function (e) { 
         e.preventDefault();
-        $(".img-fadeout").off()
+        $(".content-img").off()
     });
 
+    // Habilita un solo click para el boton info. Despues desaparece. Y habilita el boton show.
     $('#info').one("click",function (e) { 
         e.preventDefault();
-        alert("Clicar sobre uno de los emojis hara que desaparezca. FadeOut(). \n" + 
-                "Clicar sobre el boton Restaura imagenes, restaurara las imagenes. FadeIn(). \n" + 
-                "Clicar sobre el boton Muesta/Oculta hará que aparezcan/desaparezcan los emojis.\n" +
-                "Clicar sobre el boton Transparencia hará que los emojis pierdan o ganen opacidad.\n" +
-                "Clicar sobre el boton Ocultar hará que los emojis desaparezcan.\n" + 
-                "Clicar sobre el boton Mostrar hará que los emojis reaparezcan.\n" +
-                "Clicar sobre el boton Off hará que el click sobre los emojis no funcione.\n" +
-                "Clicar sobre el boton Info mostrará este alert una sola vez."
+        alert("Clicar sobre el botón ABRACADABRA hara aparecer un panel oculto. \n" + 
+                "Clicar sobre el botón ABRACADABRA dos veces, hara desaparecer el panel."
             )
+        $("#show").show(1000);
+        $(this).hide()
     });
 
-    $('#rotate').click(function (e) { 
+    //Cuando se clica sobre el boton show aparece el boton info oculto pero sin funcionalidad alguna (one()).
+    $('#show').click(function (e) { 
         e.preventDefault();
-        $('.sun').animate(
-            // {  transform: degree },
-            {degrees: 359},
-            {
-                duration: 3500,
-                easing: "linear",
-                step: function(now) {
-                    $(this).css({
-                        transform : 'rotate('+now+'deg)'
-                    });
-                }
-            }
-        );
+        $("#info").show();
     });
 
-    $('#stop').click(function (e) { 
+    $('.animation-section').prepend("<div class='section-header'><h2 class='text-center'>Anima este sol.</h2></div>");
+    //Inicia la animación de rotacion.
+    $('#rotate')
+        .click(function (e) { 
+            e.preventDefault();
+            $rotator = rotateForEver($('.sun'))
+        })
+        .focus(function (e) { 
+            e.preventDefault();
+            $('.animation-p').remove()
+            $(this).after("<div class='animation-p text-center'><p>Rotacion iniciada</p></div>")
+        });
+
+    //Para la animación de rotación.
+    $('#stop-animation').click(function (e) { 
         e.preventDefault();
-        $('.sun').stop();
+        $rotator.stop();
+        $('.animation-p').remove()
+        $(this).before("<div class='animation-p text-center'><p>Rotacion parada</p></div>")
     });
 
     $("#flip")
         .click(function(){
-            $("#panel").slideDown(2000)
+            $("#magic-panel").slideDown(2000)
         })
         .dblclick(function(){
-        $("#panel").slideUp(2000)
+        $("#magic-panel").slideUp(2000)
     });
 
     $('.social-media-links > a > i')
         .mouseenter(function () { 
-            $(this).css('color', '#23ff00');
+            $(this).css('color', '#ffff2c');
+            $(this).css('text-shadow', ' 0 0 8px #ffff0c')
         })
         .mouseout(function() {
             $(this).css('color', 'white');
-        })
+            $(this).css('text-shadow', ' 0 0 0 ')
+        });
 })
 
-$( window ).resize(function() {
-    $(".window-size").empty()
-    $(".window-size").prepend( "<div class='window-size'> <p> Width: " + $( window ).width() + "</p></div>" );
-});
+/**
+ * Función para habilitar la animación infinita de rotación.
+ * @param {*} $elem 
+ * @param {*} rotator 
+ * @returns 
+ */
+function rotateForEver($elem, rotator) {
+    if (rotator === void(0)) {
+        rotator = $({deg: 0});
+    } else {
+        rotator.get(0).deg = 0;
+    }
+
+    return rotator.animate(
+        {deg: 360},
+        {
+            duration: 5000,
+            easing: 'linear',
+            step: function(now){
+                $elem.css({transform: 'rotate(' + now + 'deg)'});
+            },
+            complete: function(){
+                rotateForEver($elem, rotator);
+            },
+        }
+    );
+}
+
+/**
+ * Función que se encarga de poner el atributo title en los elementos por parametro para que luego se visualice un tooltip.
+ * @param {*} element 
+ * @param {*} titleElement 
+ */
+function setTitle(element, titleElement) {
+    $(element).attr('title', titleElement);
+}
+
+/**
+ * Función similar a SetTitle pero en este caso para imagenes y el parent de estas.
+ * @param {} element 
+ * @param {*} parent 
+ * @param {*} titleElement 
+ * @param {*} titleParent 
+ */
+function setTitleImg(element, parent, titleElement, titleParent) {
+    $(element).each(function(){
+        setTitle('title', titleElement);
+        $(this).closest(parent).attr('title', titleParent)
+    })
+}
+
+/**
+ * Funcion que establece el estatus faded de un elemento.
+ * @param {*} element 
+ * @param {*} status 
+ */
+function setFaded(element, status) {
+    if (status){
+        $(element).fadeTo(1000, 0.4);
+        $(element).attr('faded', true)
+    } else {
+        $(element).fadeTo(1000, 1);
+        $(element).attr('faded', false)
+    }
+}
+
+/**
+ * Funcion que se encarga de poner la imagen en elelmento correspondiente y el valor de la posicion del array.
+ * @param {*} image_no 
+ * @param {*} element_img 
+ */
+function setImage (image_no, element_img) {
+    $(image_no).val(pos)
+    $(element_img).attr('src', "img/gallery/" + imagenes[pos] + ".jpg")
+}
+
+/**
+ * Establece la imagen anterior.
+ * @param {*} image_no 
+ * @param {*} element_img 
+ */
+function previousImage(image_no, element_img) {
+    pos = $(image_no).val()
+    if (pos <= 0) {
+        pos = imagenes.length -1
+        setImage(image_no, element_img)
+    } else {
+        pos = pos - 1
+        setImage(image_no, element_img)
+    }
+}
+
+/**
+ * Función que muestra la imagen siguiente.
+ * @param {*} image_no 
+ * @param {*} element_img 
+ */
+function nextImage(image_no, element_img) {
+    pos = $(image_no).val()
+    if (pos >= (imagenes.length -1)) {
+        pos = 0
+        setImage(image_no, element_img)
+    } else {
+        pos++
+        setImage(image_no, element_img)
+    }
+}
+
+/**
+ * Función que inicia el slide de las imagenes
+ * @param {*} image_no 
+ * @param {*} element_img 
+ */
+function do_slide(image_no, element_img){
+    interval = setInterval(function(){
+      nextImage(image_no, element_img);
+    }, 3000);
+}
+
+/**
+ * Función que para el slide de las imagenes. Y lo reinicia para cuando se vuelve a reproducir.
+ */
+function stop_slide(image_no){
+    clearInterval(interval)
+    pos = 0
+    $(image_no).val(pos)
+}
+
+/**
+ * Función que pausa el slide de imagenes.
+ */
+function pause_slide(){
+    clearInterval(interval)
+}
 
 /**
  * Funcion que realiza la busqueda de los editores cuando se modifica el input de texto.
@@ -186,22 +355,40 @@ $( window ).resize(function() {
     });
 }
 
-function opacityLoop(){
-    $('#logo')
+function opacityLoop(element){
+    $(element)
       .animate({opacity:0.9},1000)
-      .animate({opacity:0.2},1000, opacityLoop);
+      .animate({opacity:0.2},1000, function(){
+          opacityLoop(element)
+      });
 }
 
 /**
- * Ejercicio 1. jQuery.
+ * Pausa la animación de opacidad del logo.
+ */
+function pauseOpacityLoop(element) {
+    $(element).click(function (e) {
+        $(element).stop()
+    })
+}
+
+/**
+ * Ejercicio 1. jQuery DWEC07
+ * Obtiene una lista de editores (parte del nombre) a partir de un texto.
+ * Devuelve 10 editores máximo.
+ * @param {*} listElement 
+ * @param {*} inputElement 
  */
 function getEditorsPartNom (listElement, inputElement) {
     if (inputElement.val() != '') {
+        $('.lista').show()
         $.get(`http://${server}/editors/partNom/${inputElement.val()}`, "json", function (res) {
             $(res).each(function () {
                 createItemsList(listElement, this.nomEdit, this.idEdit)
             });
         });
+    } else {
+        $('.lista').hide()
     }
 }
 
@@ -215,7 +402,7 @@ function createItemsList (parent, element, id) {
     const modifButton = createButton("btn-warning", "Modifica", "#", id)
     const delButton = createButton("btn-danger", "Elimina", "#", id)
     let itemLi = $('<li></li>')
-    let itemDiv = $('<div class="row"></div>')
+    let itemDiv = $('<div class="flex-container"></div>')
     let text = "<div class='col'><p class='editor'>" + element + " (" + id + ")</p></div>" 
     itemDiv.append(text)
     itemDiv.append(modifButton)
