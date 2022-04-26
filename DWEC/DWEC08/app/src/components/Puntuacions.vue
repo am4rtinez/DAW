812eef8ea385e7c005e351b7ab0bb79505df1cd1
am4rtinez <anglxd@gmail.com>
2022-04-26 02:16:38 +0200

    Hacer funcionar metodos incrementar dislikes y likes

--- DWEC/DWEC08/app/src/components/Puntuacions.vue
+++ DWEC/DWEC08/app/src/components/Puntuacions.vue
@@ -1,48 +1,50 @@
 <template>
   <strong>{{ aplicacio.nom }}</strong>
   <span>
-    <button @click="incrementarFavorables">Like - {{ $store.getters.getLikes(aplicacio) }}</button>
-    <button @click="incrementarDesfavorables">Dislike - {{ $store.getters.getDislikes(aplicacio) }}</button>
+    <button type="button" @click="incrementLikes">
+      Like - {{ $store.getters.getLikes(aplicacio) }}
+    </button>
+    <button type="button" @click="incrementDislikes">
+      Dislike - {{ $store.getters.getDislikes(aplicacio) }}
+    </button>
   </span>
 </template>
 
 <script>
-  /* eslint-disable */
-  export default {
-    name: "Aplicacio",
-    props:["aplicacio"],
-    methods:{
-      incrementLikes(){
-        console.log('Like')
-        this.$store.commit("incrementarFavorables",this.$props.aplicacio);
-      },
-      incrementDislikes(){
-        console.log('Dislike')
-        this.$store.commit("incrementarDesfavorables",this.$props.aplicacio);
-      },
-    }
-  }
+/* eslint-disable */
+export default {
+  name: "Aplicacio",
+  props: ["aplicacio"],
+  methods: {
+    incrementLikes() {
+      this.$store.commit("incrementLikes", this.$props.aplicacio);
+    },
+    incrementDislikes() {
+      this.$store.commit("incrementDislikes", this.$props.aplicacio);
+    },
+  },
+};
 </script>
 
 <style scoped>
-  .aplicacio {
-    width: 30%;
-    background: #d6daa0;
-    padding: 1em;
-    margin-top: 1em;
-    position: relative;
-    left: 35%;
-    right: 35%;
-    border: 1px solid #2e2d2d;
-    border-radius: 5px;
-  }
-  .bold {
-    font-weight: bold;
-  }
-  span {
-    margin-left: 1em;
-  }
-  span button {
-    margin-right: 1em;
-  }
+.aplicacio {
+  width: 30%;
+  background: #d6daa0;
+  padding: 1em;
+  margin-top: 1em;
+  position: relative;
+  left: 35%;
+  right: 35%;
+  border: 1px solid #2e2d2d;
+  border-radius: 5px;
+}
+.bold {
+  font-weight: bold;
+}
+span {
+  margin-left: 1em;
+}
+span button {
+  margin-right: 1em;
+}
 </style>
